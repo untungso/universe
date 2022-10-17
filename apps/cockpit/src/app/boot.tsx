496f746d61a1b6b08d505ef96c8802cd39269a69
hecterbonha <untungsoandryanto@gmail.com>
2022-10-17 04:36:14 +0100

    feat(): postpone facades works

--- apps/cockpit/src/app/boot.tsx
+++ apps/cockpit/src/app/boot.tsx
@@ -1,10 +1,9 @@
 import type { ReactElement } from "react";
-import { oke } from "@instead/facades";
 
 export default function Init(): ReactElement {
   return (
     <div>
-      <div>{oke()}</div>
+      <div>---</div>
     </div>
   );
 }
--- packages/facades/src/index.ts
+++
@@ -1,1 +0,0 @@
-export const oke = (): string => "OK";
