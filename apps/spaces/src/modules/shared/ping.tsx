85acc2907e0718671cbc8eb00ab697a37eed884e
Andry <usx@Safilas-MacBook-Air.local>
2023-02-14 23:02:07 +0000

    ok

--- apps/spaces/src/modules/shared/ping.tsx
+++ apps/spaces/src/modules/shared/ping.tsx
@@ -1,18 +1,12 @@
 /**
  */
-import {
-  pingFilamentsService,
-  pingMicrosService,
-} from "../../services/ping-adapter";
 import { Button } from "../../components/button/base";
 import type { ReactElement } from "react";
 /**
  */
 const Ping = (): ReactElement => {
   const handlePing = async () => {
-    const dataFromFilaments = await pingFilamentsService();
-    const dataFromMicros = await pingMicrosService();
-    console.log(dataFromFilaments.messages, dataFromMicros.messages);
+    console.log(`dataFromFilaments.messages`, `dataFromMicros.messages`);
   };
   return (
     <div>
