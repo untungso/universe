c4f6b80d31c9def5c14192303a759c18f4047ed2
hecterbonha <untungsoandryanto@gmail.com>
2023-03-01 14:01:55 +0000

    fix turbo build

--- apps/web/src/hooks.server.ts
+++
@@ -1,8 +0,0 @@
-import { SvelteKitAuth } from "@auth/sveltekit"
-import GitHub from "@auth/core/providers/github"
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
-
-export const handle = SvelteKitAuth({
-  //@ts-ignore
-  providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
-})
