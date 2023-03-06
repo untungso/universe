3bc65bc8311954144555694a7b4481bbb08ba721
hecterbonha <untungsoandryanto@gmail.com>
2023-03-06 23:44:41 +0000

    Fix text

--- packages/components/input/text.tsx
+++ packages/components/input/text.tsx
@@ -1,13 +1,17 @@
 import * as React from "react";
 
 interface TextInputProps {
-  placeholder?:string;
+  placeholder?: string;
 }
 
 export const TextInput = (props: TextInputProps) => {
   return (
     <React.Fragment>
-      <input className="border-2 border-slate-12 rounded-md px-6 py-2 text-md placeholder:text-slate-10 font-base w-full" type="text" placeholder={props.placeholder} />
+      <input
+        className="text-md font-base w-full rounded-md border-2 border-slate-12 px-6 py-2 placeholder:text-slate-10"
+        type="text"
+        placeholder={props.placeholder}
+      />
     </React.Fragment>
   );
 };
